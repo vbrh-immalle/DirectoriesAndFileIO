3ffe2c86dc9d8d02a91e0503fa8b2ba73e0cdb49
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2016-09-28 14:38:35 +0200

    First babysteps

---
+++ DirectoriesAndFileIO/Directories.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace DirectoriesAndFileIO
+{
+    [TestClass]
+    public class Directories
+    {
+        [TestMethod]
+        public void TestGetCurrentDirectory()
+        {
+            string curdir = Directory.GetCurrentDirectory();
+            
+            Assert.AreEqual(curdir, @".");
+        }
+
+        [TestMethod]
+        public void TestGetFiles() 
+        {
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+
+            Assert.AreEqual(files.Length, 10);
+        }
+    }
+}
