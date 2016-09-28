9d184949b0d90c2f253ee8b0d17d978f24044906
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2016-09-28 15:04:40 +0200

    Add some BAD tests : often failing, hard to reproduce

--- DirectoriesAndFileIO/Directories.cs
+++ DirectoriesAndFileIO/Directories.cs
@@ -7,20 +7,41 @@ namespace DirectoriesAndFileIO
     [TestClass]
     public class Directories
     {
+        
         [TestMethod]
+        // bad test : hard to reproduce
         public void TestGetCurrentDirectory()
         {
             string curdir = Directory.GetCurrentDirectory();
-            
+
             Assert.AreEqual(@".", curdir);
         }
 
         [TestMethod]
-        public void TestGetFiles() 
+        public void TestGetFiles()
         {
             string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
 
             Assert.AreEqual(10, files.Length);
+            // TODO
         }
+
+        [TestMethod]
+        public void TestGetFilesWithSearchPattern()
+        {
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.exe");
+
+            Assert.AreEqual(10, files.Length);
+            // TODO
+        }
+
+        [TestMethod]
+        public void TestGetFilesInSubdirectories()
+        {
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(), "", SearchOption.AllDirectories);
+
+            Assert.AreEqual(10, files.Length);
+        }
+        
     }
 }
