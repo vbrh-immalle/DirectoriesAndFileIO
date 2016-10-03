e324c5f329d35a368a1574e794911a66ed0a9ce4
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2016-10-03 09:30:38 +0200

    Revert "Better comments and new test for Directory-class."
    This reverts commit ccb8327f6732e9ee5f1a598d121e03478b1807f0.

--- DirectoriesAndFileIO/TestDirectories.cs
+++ DirectoriesAndFileIO/TestDirectories.cs
@@ -10,49 +10,32 @@ namespace DirectoriesAndFileIO
         [TestInitialize]
         public void Initialize()
         {
-            // Make sure testDir is gone after possible previously failed tests
             if (Directory.Exists("testDir"))
             {
-                Directory.Delete("testDir", true);
+                Directory.Delete("testDir", true); // remove test-directory (can throw exceptions! annoying!)
             }
         }
 
         [TestCleanup]
         public void CleanUp()
         {
-            // Make sure testDir is gone after succesfully running tests
             if (Directory.Exists("testDir"))
             {
-                Directory.Delete("testDir", true); 
+                Directory.Delete("testDir", true); // remove test-directory (can throw exceptions! annoying!)
             }
         }
 
         [TestMethod]
         public void TestCreateAndDeleteDirectory()
         {
-            DirectoryInfo dir = Directory.CreateDirectory("testDir"); // Using a DirectoryInfo-object ...
+            DirectoryInfo dir = Directory.CreateDirectory("testDir");
 
             Assert.IsTrue(dir.Exists, "Directory should exist");
 
             dir.Delete();
-            dir.Refresh(); // ... we only **actually** delete here.
+            dir.Refresh(); // only here we actually delete, otherwise use Directory.Delete("testDir");
 
             Assert.IsFalse(dir.Exists, "Directory should be deleted");
-        }
-
-        [TestMethod]
-        public void TestCreateAndDeleteDirectoryDirectly()
-        {
-            // In stead of using a DirectoryInfo-object,
-            // we can use the static methods of the Directory-class directly.
-            string dirName = "testDir";
-            Directory.CreateDirectory(dirName); // we don't care about the return value here
-
-            Assert.IsTrue(Directory.Exists(dirName), "Directory should exist");
-
-            Directory.Delete(dirName);
-
-            Assert.IsFalse(Directory.Exists(dirName), "Directory should be deleted");
-        }
+        }        
     }
 }
